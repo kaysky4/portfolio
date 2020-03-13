248b79340c3bf055af94c55795a30ef8d1e0ad8c
kaysky4 <46075665+kaysky4@users.noreply.github.com>
2020-03-13 03:34:20 -0500

    Updates

--- KWPortfolio/ClientApp/src/app/app.component.ts
+++ KWPortfolio/ClientApp/src/app/app.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
-declare var gtag;
+declare let gtag;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'analytics';
-  constructor(router: Router) {
-    const navEndEvents = router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-    );
-    navEndEvents.subscribe((events: NavigationEnd) => {
-        gtag('config', 'UA-136331702-1' , {
-          'page_path': event.urlAfterRedirects
-      }); 
-    })
-  }
+    constructor(private _router: Router) {
+        this._router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                gtag('config', 'UA-136331702-1',
+                    {
+                        page_path: event.urlAfterRedirects
+                    }
+                );
+            }
+        });
+    }
 }

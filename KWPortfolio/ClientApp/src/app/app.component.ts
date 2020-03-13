a88d9b81b53359fa7b60e668d77a2f0f46b617a4
kaysky4 <46075665+kaysky4@users.noreply.github.com>
2020-03-13 01:50:20 -0500

    Google Analytics updates

--- KWPortfolio/ClientApp/src/app/app.component.ts
+++ KWPortfolio/ClientApp/src/app/app.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
-declare let gtag: Function;
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
-  constructor(public router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'xx-xxxxx-xx',
-          {
-            'page_path': event.urlAfterRedirects
-          }
-        );
-      }
-    }
-    )
+  title = 'analytics';
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+    navEndEvents.subscribe((events: NavigationEnd) => {
+        gtag('config', 'UA-136331702-1' , {
+          'page_path': event.urlAfterRedirects
+      });
+    })
   }
 }
--- KWPortfolio/ClientApp/src/app/app.module.ts
+++ KWPortfolio/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GoogleAnalyticsService } from './google-analytics.service';
 
@@ -18,6 +18,9 @@ import { FooterComponent } from './footer/footer.component';
 import { WorksModule } from './works/works.module';
 import { ResumedashModule } from './resumedash/resumedash.module';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+];
 
 @NgModule({
   declarations: [
